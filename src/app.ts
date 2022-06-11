f18b11e10ee71ae4d2db2cd7deb0d86da82a75b3
Dine Mustapha Kamel <musk96@hotmail.fr>
2022-06-11 01:18:45 +0100

    simple refactor

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,20 @@
 import express from "express";
+import { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler } from "./middlewares";
 import authRouter from "./routes/auth";
 import passport from "passport";
+import jwt from "jsonwebtoken";
 
 const app = express();
 app.use(json());
 
 app.use(
   cookieSession({
-    signed: false,
-    secure: false,
+    // signed: false,
+    // secure: false,
     maxAge: 24 * 60 * 60 * 1000,
     keys: [process.env.COOKIE_KEY!],
   })
@@ -23,7 +25,23 @@ app.use(passport.session());
 app.use(currentUser);
 
 app.use("/api/v1/auth", authRouter);
-app.get("/auth/google/callback", passport.authenticate("google"));
+
+app.get("/auth/google/callback", passport.authenticate("google", {scope: ["email", "profile"]}),
+function(req, res) {
+  
+  const userJwt = jwt.sign(
+    {
+      id: req.user?.id,
+      email: req.user?.email,
+    },
+    process.env.JWT_KEY!
+  );
+
+  req.session = {
+    jwt: userJwt
+  };
+    res.send(req.user);
+});
 
 app.use(errorHandler);
 
