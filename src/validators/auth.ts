c54a7398c3ef5e0ce9a4af1b27191c5536735d89
Dine Mustapha Kamel <musk96@hotmail.fr>
2022-06-11 23:26:20 +0100

    new validation middlewares

--- src/validators/auth.ts
+++ src/validators/auth.ts
@@ -1,8 +1,12 @@
-import { body } from "express-validator";
+import { body, check, param } from "express-validator";
 
 export const signInValidator = [
   body("email").isEmail().withMessage("Email must be valid"),
-  body("password").trim().notEmpty().isLength({ min: 6 }).withMessage("you must supply a valid password"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6, max: 50 })
+    .withMessage("you must supply a valid password"),
 ];
 
 export const signUpValidator = [
@@ -13,5 +17,48 @@ export const signUpValidator = [
     .isLength({ min: 1, max: 25 })
     .withMessage("you must supply a valid last name"),
   body("email").isEmail().withMessage("Email must be valid"),
-  body("password").trim().notEmpty().isLength({ min: 6 }).withMessage("you must supply a valid password"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6, max: 50 })
+    .withMessage("you must supply a valid password"),
+];
+
+export const updateUserValidator = [
+  param("id").isMongoId().withMessage("Invalid ID"),
+  body("firstName")
+    .isLength({ min: 1, max: 25 })
+    .optional()
+    .withMessage("you must supply a valid first name"),
+  body("lastName")
+    .isLength({ min: 1, max: 25 })
+    .optional()
+    .withMessage("you must supply a valid last name"),
+  body("email").optional().isEmail().withMessage("Email must be valid"),
+  check("password").not().exists().withMessage("Invalid Input"),
+];
+
+export const updatePasswordValidator = [
+  param("id").isMongoId().withMessage("Invalid ID"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6, max: 50 })
+    .withMessage("you must supply a valid password"),
+  body("newPassword")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 6, max: 50 })
+    .withMessage("you must supply a valid password"),
+];
+
+export const userIdValidator = [
+  param("id").isMongoId().withMessage("Invalid ID"),
+];
+
+export const findUsersValidator = [
+  param("id").isMongoId().withMessage("Invalid ID"),
+  body("input")
+    .isLength({ min: 1, max: 50 })
+    .withMessage("you must supply a valid full name"),
 ];
